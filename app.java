0fd6f3c01e7287f940573cef074d562dc0a1374e
Karthik7123 <109735867+Karthik7123@users.noreply.github.com>
2025-11-18 00:21:31 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+
+        // Declare and initialize two integer variables
+        int number1 = 10;
+        int number2 = 5;
+
+        // Calculate the sum of the two numbers
+        int sum = number1 + number2;
+
+        // Print the result of the calculation
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
